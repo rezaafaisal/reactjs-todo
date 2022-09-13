f5042b6eb438ad3cd7d2b5307715a425c03599eb
rezafaisal98 <rezha.faizal567@gmail.com>
2022-09-14 01:18:18 +0800

    boostrap vs MUI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { Button } from "@mui/material";
+import Header from "./components/Header";
+import { TextField } from "@mui/material";
 function App() {
   return (
     <div className="App">
+      <Header />
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <div className="col-12 col-md-8 col-lg-6">
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+            />
+            <Button variant="contained">halo</Button>
+          </div>
+        </div>
+      </div>
       Halo Bang aowkoawko
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <div className="d-flex justify-content-center w-100 py-4 bg-info">
+         <span className="fs-4 text-white fw-bold">To Do App</span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
